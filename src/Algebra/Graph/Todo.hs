e11bea4eb712e9cc0e3b9b7ca153909866b483b6
Andrey Mokhov <andrey.mokhov@gmail.com>
2016-12-21 18:37:43 +0000

    Generalise priorities

--- src/Algebra/Graph/Todo.hs
+++ src/Algebra/Graph/Todo.hs
@@ -1,12 +1,14 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Algebra.Graph.Todo (Todo, todo, low, high, (><)) where
 
+import           Data.Map (Map)
+import qualified Data.Map as Map
 import Data.String
 
 import Algebra.Graph
 import Algebra.Graph.Util
 
-data Todo a = T [a] [a] (TopSort a) deriving Show
+data Todo a = T (Map a Int) (TopSort a) deriving Show
 
 instance Ord a => Eq (Todo a) where
     x == y = todo x == todo y
@@ -14,23 +16,20 @@ instance Ord a => Eq (Todo a) where
 instance (IsString a, Ord a) => IsString (Todo a) where
     fromString = vertex . fromString
 
-low :: Ord a => a -> Todo a
-low x = T [x] [] $ vertex x
+low :: Todo a -> Todo a
+low (T p g) = T (Map.map (subtract 1) p) g
 
-high :: Ord a => a -> Todo a
-high x = T [] [x] $ vertex x
+high :: Todo a -> Todo a
+high (T p g) = T (Map.map (+1) p) g
 
-(><) :: Ord a => a -> a -> Todo a
+(><) :: Ord a => Todo a -> Todo a -> Todo a
 x >< y = low x `connect` high y
 
 todo :: forall a. Ord a => Todo a -> Maybe [a]
-todo (T l h g) = fmap (map snd) . topSort $ mapVertices prioritise g
+todo (T p g) = fmap (map snd) . topSort $ mapVertices prioritise g
   where
     prioritise :: a -> (Int, a)
-    prioritise x
-        | x `elem` h = (-1, x)
-        | x `elem` l = ( 1, x)
-        | otherwise  = ( 0, x)
+    prioritise x = (negate $ Map.findWithDefault 0 x p, x)
 
 instance (IsString a, Ord a) => Num (Todo a) where
     fromInteger i = fromString $ show (fromInteger i :: Integer)
@@ -42,7 +41,7 @@ instance (IsString a, Ord a) => Num (Todo a) where
 
 instance Ord a => Graph (Todo a) where
     type Vertex (Todo a) = a
-    empty  = T [] [] empty
-    vertex = T [] [] . vertex
-    overlay (T l1 h1 g1) (T l2 h2 g2) = T (l1 ++ l2) (h1 ++ h2) (overlay g1 g2)
-    connect (T l1 h1 g1) (T l2 h2 g2) = T (l1 ++ l2) (h1 ++ h2) (connect g1 g2)
+    empty    = T Map.empty empty
+    vertex x = T (Map.singleton x 0) (vertex x)
+    overlay (T p1 g1) (T p2 g2) = T (Map.unionWith (+) p1 p2) (overlay g1 g2)
+    connect (T p1 g1) (T p2 g2) = T (Map.unionWith (+) p1 p2) (connect g1 g2)
