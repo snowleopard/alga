5de11559b1c818c336c8e86ba59e4e5ee95a86e9
Andrey Mokhov <andrey.mokhov@gmail.com>
2017-03-12 03:43:35 +0000

    Improve documentation

--- src/Algebra/Graph.hs
+++ src/Algebra/Graph.hs
@@ -33,37 +33,47 @@ import Algebra.Graph.Classes
 
 -- | Construct the graph comprising a given list of isolated vertices.
 --
--- > vertices []  == empty
--- > vertices [x] == vertex x
+-- @
+-- vertices []  == 'empty'
+-- vertices [x] == 'vertex' x
+-- @
 vertices :: Graph g => [Vertex g] -> g
 vertices = overlays . map vertex
 
 -- | Overlay a given list of graphs.
 --
--- > overlays []     == empty
--- > overlays [x]    == x
--- > overlays [x, y] == overlay x y
+-- @
+-- overlays []     == 'empty'
+-- overlays [x]    == x
+-- overlays [x, y] == 'overlay' x y
+-- @
 overlays :: Graph g => [g] -> g
 overlays = foldr overlay empty
 
 -- | Connect a given list of graphs.
 --
--- > connects []     == empty
--- > connects [x]    == x
--- > connects [x, y] == connect x y
+-- @
+-- connects []     == 'empty'
+-- connects [x]    == x
+-- connects [x, y] == 'connect' x y
+-- @
 connects :: Graph g => [g] -> g
 connects = foldr connect empty
 
 -- | Construct the graph comprising a single edge.
 --
--- > edge x y == connect (vertex x) (vertex y)
+-- @
+-- edge x y == 'connect' ('vertex' x) ('vertex' y)
+-- @
 edge :: Graph g => Vertex g -> Vertex g -> g
 edge x y = connect (vertex x) (vertex y)
 
 -- | Construct the graph from a list of edges.
 --
--- > edges []       == empty
--- > edges [(x, y)] == edge x y
+-- @
+-- edges []       == 'empty'
+-- edges [(x, y)] == 'edge' x y
+-- @
 edges :: Graph g => [(Vertex g, Vertex g)] -> g
 edges = overlays . map (uncurry edge)
 
@@ -71,22 +81,33 @@ edges = overlays . map (uncurry edge)
 -- The resulting graph contains both the vertices @V@ and all the vertices
 -- referred to by the edges @E@.
 --
--- > graph []  []       == empty
--- > graph [x] []       == vertex x
--- > graph []  [(x, y)] == edge x y
+-- @
+-- graph []  []       == 'empty'
+-- graph [x] []       == 'vertex' x
+-- graph []  [(x, y)] == 'edge' x y
+-- @
 graph :: Graph g => [Vertex g] -> [(Vertex g, Vertex g)] -> g
 graph vs es = overlay (vertices vs) (edges es)
 
 -- | The 'isSubgraphOf' function takes two graphs and returns 'True' if the
 -- first graph is a /subgraph/ of the second.
+--
+-- @
+-- isSubgraphOf 'empty' x                     == True
+-- isSubgraphOf ('vertex' x) 'empty'            == False
+-- isSubgraphOf x ('overlay' x y)             == True
+-- isSubgraphOf ('overlay' x y) ('connect' x y) == True
+-- @
 isSubgraphOf :: (Graph g, Eq g) => g -> g -> Bool
 isSubgraphOf x y = overlay x y == y
 
 -- | The /path/ on a list of vertices.
 --
--- > path []     == empty
--- > path [x]    == vertex x
--- > path [x, y] == edge x y
+-- @
+-- path []     == 'empty'
+-- path [x]    == 'vertex' x
+-- path [x, y] == 'edge' x y
+-- @
 path :: Graph g => [Vertex g] -> g
 path []  = empty
 path [x] = vertex x
@@ -94,36 +115,44 @@ path xs  = edges $ zip xs (tail xs)
 
 -- | The /circuit/ on a list of vertices.
 --
--- > circuit []     == empty
--- > circuit [x]    == edge x x
--- > circuit [x, y] == edges [(x, y), (y, x)]
+-- @
+-- circuit []     == 'empty'
+-- circuit [x]    == 'edge' x x
+-- circuit [x, y] == 'edges' [(x, y), (y, x)]
+-- @
 circuit :: Graph g => [Vertex g] -> g
 circuit []     = empty
 circuit (x:xs) = path $ [x] ++ xs ++ [x]
 
 -- | The /clique/ on a list of vertices.
 --
--- > clique []        == empty
--- > clique [x]       == vertex x
--- > clique [x, y]    == edge x y
--- > clique [x, y, z] == edges [(x, y), (x, z), (y, z)]
+-- @
+-- clique []        == 'empty'
+-- clique [x]       == 'vertex' x
+-- clique [x, y]    == 'edge' x y
+-- clique [x, y, z] == 'edges' [(x, y), (x, z), (y, z)]
+-- @
 clique :: Graph g => [Vertex g] -> g
 clique = connects . map vertex
 
 -- | The /biclique/ on a list of vertices.
 --
--- > biclique []       []       == empty
--- > biclique [x]      []       == vertex x
--- > biclique []       [y]      == vertex y
--- > biclique [x1, x2] [y1, y2] == edges [(x1, y1), (x1, y2), (x2, y1), (x2, y2)]
+-- @
+-- biclique []       []       == 'empty'
+-- biclique [x]      []       == 'vertex' x
+-- biclique []       [y]      == 'vertex' y
+-- biclique [x1, x2] [y1, y2] == 'edges' [(x1, y1), (x1, y2), (x2, y1), (x2, y2)]
+-- @
 biclique :: Graph g => [Vertex g] -> [Vertex g] -> g
 biclique xs ys = connect (vertices xs) (vertices ys)
 
 -- | The /star/ formed by a centre vertex and a list of leaves.
 --
--- > star x []     == vertex x
--- > star x [y]    == edge x y
--- > star x [y, z] == edges [(x, y), (x, z)]
+-- @
+-- star x []     == 'vertex' x
+-- star x [y]    == 'edge' x y
+-- star x [y, z] == 'edges' [(x, y), (x, z)]
+-- @
 star :: Graph g => Vertex g -> [Vertex g] -> g
 star x ys = connect (vertex x) (vertices ys)
 
