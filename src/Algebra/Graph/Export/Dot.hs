2902001b94b3bbf46a5696764ff412fe198a3cf2
Andrey Mokhov <andrey.mokhov@gmail.com>
2017-07-23 02:51:50 +0100

    Add examples
    See #16

--- src/Algebra/Graph/Export/Dot.hs
+++ src/Algebra/Graph/Export/Dot.hs
@@ -14,7 +14,6 @@
 -- This module defines functions for exporting graphs in DOT file format.
 -----------------------------------------------------------------------------
 module Algebra.Graph.Export.Dot (
-    -- * Exporting graphs as DOT documents
     Attribute (..), Style (..), defaultStyle, defaultStyleViaShow, exportWith,
     export, exportViaShow
   ) where
@@ -33,7 +32,7 @@ import Algebra.Graph.Class (ToGraph (..))
 -- Attributes are used to specify the style of graph elements during export.
 data Attribute s = (:=) s s
 
--- | Example: @dotAttributes ["label" := "A label", "shape" := "box"]@
+-- Example: @dotAttributes ["label" := "A label", "shape" := "box"]@
 -- corresponds to document: @ [label="A label" shape="box"]@.
 dotAttributes :: IsString s => [Attribute s] -> Doc s
 dotAttributes [] = mempty
@@ -41,13 +40,17 @@ dotAttributes as = indent 1 . brackets . mconcat . intersperse " " $ map dot as
   where
     dot (k := v) = mconcat [literal k <> "=" <> doubleQuotes (literal v)]
 
--- | Everything we need to know to export a graph in the DOT format.
+-- | The record 'Style' @a@ @s@ specifies the style to use when exporting a
+-- graph in the DOT format. Here @a@ is the type of the graph vertices, and @s@
+-- is the type of string to represent the resulting DOT document (e.g. String,
+-- ByteString, etc.). Most fields can be empty. The only field that has no
+-- obvious default value is 'vertexName', which holds a function of type
+-- @a -> s@ to compute vertex names. See the example for the function 'exportWith'.
 data Style a s = Style
     { graphName :: s
-    -- ^ The name of the graph.
+    -- ^ Name of the graph.
     , preamble :: s
-    -- ^ Preamble is added at the beginning of the DOT file body, after the
-    -- opening @{@ bracket.
+    -- ^ Preamble is added at the beginning of the DOT file body.
     , graphAttributes :: [Attribute s]
     -- ^ Graph style, e.g. @["bgcolor" := "azure"]@.
     , defaultVertexAttributes :: [Attribute s]
@@ -62,34 +65,108 @@ data Style a s = Style
     -- ^ Attributes of a specific edge.
     }
 
--- | Default style for exporting graphs.
+-- | Default style for exporting graphs. All style settings are empty except for
+-- 'vertexName', which is provided as the only argument.
 defaultStyle :: Monoid s => (a -> s) -> Style a s
 defaultStyle v = Style mempty mempty [] [] [] v (\_ -> []) (\_ _ -> [])
 
--- | Default style for exporting graphs whose vertices are 'Show'-able.
+-- | Default style for exporting graphs whose vertices are 'Show'-able. All
+-- style settings are empty except for 'vertexName', which is computed from
+-- 'show'.
+--
+-- @
+-- defaultStyleViaShow = 'defaultStyle' ('fromString' . 'show')
+-- @
 defaultStyleViaShow :: (Show a, IsString s, Monoid s) => Style a s
 defaultStyleViaShow = defaultStyle (fromString . show)
 
 -- TODO: Avoid round-trip graph conversion if g :: AdjacencyMap a.
--- | Export a graph using a given DOT style.
-exportWith :: (IsString s, Monoid s, Ord a, ToGraph g, ToVertex g ~ a) => Style a s -> g -> s
+-- | Export a graph with a given style.
+--
+-- For example:
+--
+-- @
+-- style :: 'Style' Int String
+-- style = 'Style'
+--     { 'graphName'               = \"Example\"
+--     , 'preamble'                = "  // This is an example\\n"
+--     , 'graphAttributes'         = ["label" := \"Example\", "labelloc" := "top"]
+--     , 'defaultVertexAttributes' = ["shape" := "circle"]
+--     , 'defaultEdgeAttributes'   = 'mempty'
+--     , 'vertexName'              = \x   -> "v" ++ 'show' x
+--     , 'vertexAttributes'        = \x   -> ["color" := "blue"   | 'odd' x      ]
+--     , 'edgeAttributes'          = \x y -> ["style" := "dashed" | 'odd' (x * y)] }
+--
+-- > putStrLn $ exportWith style (1 * 2 + 3 * 4 * 5 :: 'Graph' Int)
+--
+-- digraph Example
+-- {
+--   // This is an example
+--
+--   graph [label=\"Example\" labelloc="top"]
+--   node [shape="circle"]
+--   "v1" [color="blue"]
+--   "v2"
+--   "v3" [color="blue"]
+--   "v4"
+--   "v5" [color="blue"]
+--   "v1" -> "v2"
+--   "v3" -> "v4"
+--   "v3" -> "v5" [style="dashed"]
+--   "v4" -> "v5"
+-- }
+-- @
+exportWith :: (IsString s, Monoid s, Eq s, Ord a, ToGraph g, ToVertex g ~ a) => Style a s -> g -> s
 exportWith Style {..} g = E.export $ unlines $ header ++ map (indent 2) body ++ ["}"]
   where
-    header    = ["digraph " <> literal graphName, "{", literal preamble]
-             ++ ("graph" `with` graphAttributes)
+    header    = ["digraph " <> literal graphName, "{"]
+             ++ [ literal preamble | preamble /= mempty ]
+    with x as = if null as then [] else [x <> dotAttributes as]
+    body      = ("graph" `with` graphAttributes)
              ++ ("node"  `with` defaultVertexAttributes)
              ++ ("edge"  `with` defaultEdgeAttributes)
-    with x as = if null as then [] else [x <> dotAttributes as]
-    body      = map v (vertexList adj) ++ map e (edgeList adj)
+             ++ map v (vertexList adj) ++ map e (edgeList adj)
     label     = doubleQuotes . literal . vertexName
     v x       = label x <> dotAttributes (vertexAttributes x)
     e (x, y)  = label x <> " -> " <> label y <> dotAttributes (edgeAttributes x y)
     adj       = toGraph g
 
--- | Export a graph using the 'defaultStyle'.
+-- | Export a graph whose vertices are represented simply by their names.
+--
+-- For example:
+--
+-- @
+-- > ByteString.putStrLn $ export ('circuit' ["a", "b", "c"] :: 'AdjacencyMap' ByteString)
+--
+-- digraph
+-- {
+--   "a"
+--   "b"
+--   "c"
+--   "a" -> "b"
+--   "b" -> "c"
+--   "c" -> "a"
+-- }
+-- @
 export :: (IsString s, Monoid s, Ord s, ToGraph g, ToVertex g ~ s) => g -> s
 export = exportWith (defaultStyle id)
 
 -- | Export a graph using the 'defaultStyleViaShow'.
-exportViaShow :: (IsString s, Monoid s, ToGraph g, Ord (ToVertex g), Show (ToVertex g)) => g -> s
+--
+-- For example:
+--
+-- @
+-- > putStrLn $ exportViaShow (1 + 2 * (3 + 4) :: 'Graph' Int)
+--
+-- digraph
+-- {
+--   "1"
+--   "2"
+--   "3"
+--   "4"
+--   "2" -> "3"
+--   "2" -> "4"
+-- }
+-- @
+exportViaShow :: (IsString s, Monoid s, Eq s, ToGraph g, Ord (ToVertex g), Show (ToVertex g)) => g -> s
 exportViaShow = exportWith defaultStyleViaShow
