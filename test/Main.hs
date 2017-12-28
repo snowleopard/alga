d42f7e55831518a93e7df11e695e805348ef81db
Andrey Mokhov <andrey.mokhov@gmail.com>
2017-12-28 03:45:40 +0300

    Move Doc datatype to Utilities

--- test/Main.hs
+++ test/Main.hs
@@ -5,6 +5,7 @@ import Algebra.Graph.Test.Graph
 import Algebra.Graph.Test.IntAdjacencyMap
 import Algebra.Graph.Test.NonEmptyGraph
 import Algebra.Graph.Test.Relation
+import Algebra.Graph.Test.Utilities
 
 main :: IO ()
 main = do
@@ -15,3 +16,4 @@ main = do
     testIntAdjacencyMap
     testNonEmptyGraph
     testRelation
+    testUtilities
