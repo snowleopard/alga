53a129dad47e84c46f4edd1c9e1995d92e37eda9
Andrey Mokhov <andrey.mokhov@gmail.com>
2018-04-24 02:09:45 +0100

    Add the Distance dioid and minor revision

--- src/Algebra/Graph/Labelled.hs
+++ src/Algebra/Graph/Labelled.hs
@@ -16,10 +16,14 @@
 -----------------------------------------------------------------------------
 module Algebra.Graph.Labelled (
     -- * Algebraic data type for edge-labeleld graphs
-    Dioid (..), Graph (..), UnlabelledGraph,
+    Dioid (..), Graph (..), UnlabelledGraph, overlay, connect, lconnect,
+    (*<), (>*),
+
+    -- * Distances
+    Distance (..),
 
     -- * Operations
-    edgeWeight
+    edgeLabel
   ) where
 
 import Prelude ()
@@ -55,14 +59,30 @@ infixl 7 |*|
 -- Type variable e stands for edge labels
 data Graph e a = Empty
                | Vertex a
-               | LabelledConnect e (Graph e a) (Graph e a)
+               | LConnect e (Graph e a) (Graph e a)
                deriving (Foldable, Functor, Show, Traversable)
 
 overlay :: Dioid e => Graph e a -> Graph e a -> Graph e a
-overlay = LabelledConnect zero
+overlay = LConnect zero
 
 connect :: Dioid e => Graph e a -> Graph e a -> Graph e a
-connect = LabelledConnect one
+connect = LConnect one
+
+lconnect :: e -> Graph e a -> Graph e a -> Graph e a
+lconnect = LConnect
+
+-- Convenient ternary-ish operator x *<e>* y, for example:
+-- x = Vertex "x"
+-- y = Vertex "y"
+-- z = x *<1>* y
+(*<) :: Graph e a -> e -> (Graph e a, e)
+g *< e = (g, e)
+
+(>*) :: (Graph e a, e) -> Graph e a -> Graph e a
+(g, e) >* h = LConnect e g h
+
+infixl 5 *<
+infixl 5 >*
 
 -- TODO: Prove the C.Graph laws
 instance Dioid e => C.Graph (Graph e a) where
@@ -72,10 +92,10 @@ instance Dioid e => C.Graph (Graph e a) where
     overlay = overlay
     connect = connect
 
-edgeWeight :: (Eq a, Dioid e) => a -> a -> Graph e a -> e
-edgeWeight _ _ Empty                   = zero
-edgeWeight _ _ (Vertex _)              = zero
-edgeWeight x y (LabelledConnect e g h) = edgeWeight x y g |+| edgeWeight x y h |+| new
+edgeLabel :: (Eq a, Dioid e) => a -> a -> Graph e a -> e
+edgeLabel _ _ Empty            = zero
+edgeLabel _ _ (Vertex _)       = zero
+edgeLabel x y (LConnect e g h) = edgeLabel x y g |+| edgeLabel x y h |+| new
   where
     new | x `elem` g && y `elem` h = e
         | otherwise                = zero
@@ -88,3 +108,35 @@ instance Dioid Bool where
 
 -- TODO: Prove that this is identical to Algebra.Graph
 type UnlabelledGraph a = Graph Bool a
+
+data Distance a = Finite a | Infinite deriving (Eq, Ord, Show)
+
+instance (Ord a, Num a) => Num (Distance a) where
+    fromInteger = Finite . fromInteger
+
+    Infinite + _ = Infinite
+    _ + Infinite = Infinite
+    Finite x + Finite y = Finite (x + y)
+
+    Infinite * _ = Infinite
+    _ * Infinite = Infinite
+    Finite x * Finite y = Finite (x * y)
+
+    negate _ = error "Negative distances not allowed"
+
+    signum (Finite 0) = 0
+    signum _ = 1
+
+    abs = id
+
+instance (Num a, Ord a) => Dioid (Distance a) where
+    zero = Infinite
+    one  = Finite 0
+
+    Infinite |+| x = x
+    x |+| Infinite = x
+    Finite x |+| Finite y = Finite (min x y)
+
+    Infinite |*| _ = Infinite
+    _ |*| Infinite = Infinite
+    Finite x |*| Finite y = Finite (x + y)
