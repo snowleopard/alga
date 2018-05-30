5699c260000b76abcabb21cbdbef3fec50a77168
Alexandre Moine <alexandre@moine.me>
2018-05-30 20:49:57 +0200

    Use definition from HigherKinded in Graph

--- src/Algebra/Graph.hs
+++ src/Algebra/Graph.hs
@@ -437,7 +437,7 @@ hasVertex = H.hasVertex
 -- hasEdge x y                  == 'elem' (x,y) . 'edgeList'
 -- @
 hasEdge :: Ord a => a -> a -> Graph a -> Bool
-hasEdge = H.hasEdge
+hasEdge u v = (edge u v `isSubgraphOf`) . induce (`elem` [u, v])
 
 -- | The number of vertices in a graph.
 -- Complexity: /O(s * log(n))/ time.
@@ -694,7 +694,7 @@ deBruijn = H.deBruijn
 -- removeVertex x . removeVertex x == removeVertex x
 -- @
 removeVertex :: Eq a => a -> Graph a -> Graph a
-removeVertex = H.removeVertex
+removeVertex v = induce (/= v)
 
 -- | Remove an edge from a given graph.
 -- Complexity: /O(s)/ time, memory and size.
