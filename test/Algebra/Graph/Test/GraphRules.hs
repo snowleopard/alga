fe9d6089ab972644d8b778b06118a5a20d663952
Alexandre Moine <alexandre@moine.me>
2018-10-20 17:50:17 +0200

    Add compilation-time inspection

---
+++ test/Algebra/Graph/Test/GraphRules.hs
@@ -0,0 +1,26 @@
+{-# LANGUAGE TemplateHaskell #-}
+-----------------------------------------------------------------------------
+-- |
+-- Module     : Algebra.Graph.Test.Graph
+-- Copyright  : (c) Andrey Mokhov 2016-2018
+-- License    : MIT (see the file LICENSE)
+-- Maintainer : andrey.mokhov@gmail.com
+-- Stability  : experimental
+--
+-- Testsuite for "Algebra.Graph" rewrite-rules
+-----------------------------------------------------------------------------
+module Algebra.Graph.Test.GraphRules where
+
+import Algebra.Graph
+
+import qualified Test.Inspection as I
+
+-- transpose . star
+starTranspose :: Int -> [Int] -> Graph Int
+starTranspose a [] = vertex a
+starTranspose a xs = connect (vertices xs) (vertex a)
+
+transposeDotStar :: Int -> [Int] -> Graph Int
+transposeDotStar x = transpose . star x
+
+I.inspect $ 'starTranspose I.=== 'transposeDotStar
