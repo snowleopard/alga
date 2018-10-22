942f8c835b8d28bdab79d9a9797b7572fa704a88
Alexandre Moine <alexandre@moine.me>
2018-10-22 13:28:59 +0200

    Factorize some code

--- test/Algebra/Graph/Test/GraphRules.hs
+++ test/Algebra/Graph/Test/GraphRules.hs
@@ -17,48 +17,43 @@ import qualified Test.Inspection as I
 
 -- transpose tests
 --- transpose . star
-starTranspose :: a -> [a] -> Graph a
+starTranspose, transposeDotStar :: a -> [a] -> Graph a
 starTranspose a [] = vertex a
 starTranspose a xs = connect (vertices xs) (vertex a)
 
-transposeDotStar :: a -> [a] -> Graph a
 transposeDotStar x = transpose . star x
 
 I.inspect $ 'starTranspose I.=== 'transposeDotStar
 
 --- transpose . overlays
-overlays' :: [Graph a] -> Graph a
-overlays' = overlays . map transpose
+overlays', transposeDotOverlays :: [Graph a] -> Graph a
+overlays'            = overlays . map transpose
 
-transposeDotOverlays :: [Graph a] -> Graph a
 transposeDotOverlays = transpose . overlays
 
 I.inspect $ 'overlays' I.=== 'transposeDotOverlays
 
 --- transpose . vertices
-vertices' :: [a] -> Graph a
-vertices' = overlays . map vertex
+vertices', transposeDotVertices :: [a] -> Graph a
+vertices'            = overlays . map vertex
 
-transposeDotVertices :: [a] -> Graph a
 transposeDotVertices = transpose . vertices
 
 I.inspect $ 'vertices' I.=== 'transposeDotVertices
 
 -- buildG tests
 --- foldg . fmap fusion
-foldgFmap :: b -> (a -> b) -> (b -> b -> b) -> (b -> b -> b) -> (a -> a) -> Graph a -> b
-foldgFmap e v o c f = foldg e (v . f) o c
+foldgFmap, foldgDotFmap :: b -> (a -> b) -> (b -> b -> b) -> (b -> b -> b) -> (a -> a) -> Graph a -> b
+foldgFmap    e v o c f = foldg e (v . f) o c
 
-foldgDotFmap :: b -> (a -> b) -> (b -> b -> b) -> (b -> b -> b) -> (a -> a) -> Graph a -> b
 foldgDotFmap e v o c f = foldg e v o c . fmap f
 
 I.inspect $ 'foldgFmap I.=== 'foldgDotFmap
 
 --- foldg . transpose fusion
-foldgTranspose :: b -> (a -> b) -> (b -> b -> b) -> (b -> b -> b) -> Graph a -> b
-foldgTranspose e v o c = foldg e v o (flip c)
+foldgTranspose, foldgDotTranspose :: b -> (a -> b) -> (b -> b -> b) -> (b -> b -> b) -> Graph a -> b
+foldgTranspose    e v o c = foldg e v o (flip c)
 
-foldgDotTranspose :: b -> (a -> b) -> (b -> b -> b) -> (b -> b -> b) -> Graph a -> b
 foldgDotTranspose e v o c = foldg e v o c . transpose
 
 I.inspect $ 'foldgTranspose I.=== 'foldgDotTranspose
