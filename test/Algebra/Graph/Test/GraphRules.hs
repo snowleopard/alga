4bc938f2422f03c1e2794fd221e6f48c406f0f65
Alexandre Moine <alexandre@moine.me>
2018-10-24 17:31:42 +0200

    Even more tests

--- test/Algebra/Graph/Test/GraphRules.hs
+++ test/Algebra/Graph/Test/GraphRules.hs
@@ -18,35 +18,82 @@ import Algebra.Graph.Internal
 
 import qualified Test.Inspection as I
 
--- overlays tests
+-- overlays/connects tests
+--- overlays . map vertex
 vertices', overlaysDotMapVertex :: [a] -> Graph a
 vertices'            = fromMaybe Empty . foldr (mf Overlay . Vertex) Nothing
 overlaysDotMapVertex = overlays . map vertex
 
 I.inspect $ 'vertices' I.=== 'overlaysDotMapVertex
 
+--- overlays . map vertex
+clique', connectsDotMapVertex :: [a] -> Graph a
+clique'              = fromMaybe Empty . foldr (mf Connect . Vertex) Nothing
+connectsDotMapVertex = connects . map vertex
+
+I.inspect $ 'clique' I.=== 'connectsDotMapVertex
+
 -- transpose tests
---- transpose . star
-starTranspose, transposeDotStar :: a -> [a] -> Graph a
-starTranspose a [] = vertex a
-starTranspose a xs = connect (vertices xs) (vertex a)
+--- transpose empty
+empty', transposeEmpty :: Graph a
+empty'         = Empty
+transposeEmpty = transpose Empty
 
-transposeDotStar x = transpose . star x
+I.inspect $ 'empty' I.=== 'transposeEmpty
 
-I.inspect $ 'starTranspose I.=== 'transposeDotStar
+--- transpose . vertex
+vertex', transposeDotVertex :: a -> Graph a
+vertex'            = Vertex
+transposeDotVertex = transpose . vertex
+
+I.inspect $ 'vertex' I.=== 'transposeDotVertex
+
+--- transpose . overlay
+overlayTransposed, transposeDotOverlay :: Graph a -> Graph a -> Graph a
+overlayTransposed   g1 g2 = Overlay (transpose g1) (transpose g2)
+transposeDotOverlay g1 g2 = transpose $ Overlay g1 g2
+
+I.inspect $ 'overlayTransposed I.=== 'transposeDotOverlay
+
+--- transpose . connect
+connectTransposed, transposeDotConnect :: Graph a -> Graph a -> Graph a
+connectTransposed   g1 g2 = Connect (transpose g2) (transpose g1)
+transposeDotConnect g1 g2 = transpose $ Connect g1 g2
+
+I.inspect $ 'connectTransposed I.=== 'transposeDotConnect
 
 --- transpose . overlays
-overlays', transposeDotOverlays :: [Graph a] -> Graph a
-overlays'            = overlays . map transpose
-
+overlaysTransposed, transposeDotOverlays :: [Graph a] -> Graph a
+overlaysTransposed   = overlays . map transpose
 transposeDotOverlays = transpose . overlays
 
-I.inspect $ 'overlays' I.=== 'transposeDotOverlays
+I.inspect $ 'overlaysTransposed I.=== 'transposeDotOverlays
+
+--- transpose . connects
+connectsTransposed, transposeDotConnects :: [Graph a] -> Graph a
+connectsTransposed   = connects . reverse . map transpose
+transposeDotConnects = transpose . connects
+
+I.inspect $ 'connectsTransposed I.=== 'transposeDotConnects
 
 --- transpose . vertices
 verticesTransposed, transposeDotVertices :: [a] -> Graph a
 verticesTransposed   = overlays . map vertex
-
 transposeDotVertices = transpose . overlays . map vertex
 
 I.inspect $ 'verticesTransposed I.=== 'transposeDotVertices
+
+--- transpose . clique
+cliqueTransposed, transposeDotClique :: [a] -> Graph a
+cliqueTransposed   = connects . reverse . map vertex
+transposeDotClique = transpose . connects . map vertex
+
+I.inspect $ 'cliqueTransposed I.=== 'transposeDotClique
+
+--- transpose . star
+starTranspose, transposeDotStar :: a -> [a] -> Graph a
+starTranspose a [] = vertex a
+starTranspose a xs = connect (vertices xs) (vertex a)
+transposeDotStar x = transpose . star x
+
+I.inspect $ 'starTranspose I.=== 'transposeDotStar
