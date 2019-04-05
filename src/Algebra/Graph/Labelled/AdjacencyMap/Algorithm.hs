4fe399afbeb44242a6ce674cb0d9e96e41ba1b0f
adithyaov <111501017@smail.iitpkd.ac.in>
2019-04-05 22:51:06 +0530

    Made the dijkstra algorithm, unfortunately no monoid instance of edge labels

--- src/Algebra/Graph/Labelled/AdjacencyMap/Algorithm.hs
+++ src/Algebra/Graph/Labelled/AdjacencyMap/Algorithm.hs
@@ -1,39 +1,63 @@
-module Algebra.Graph.Labelled.AdjacencyMap.Algorithm where 
+module Algebra.Graph.Labelled.AdjacencyMap.Algorithm where
+
 import qualified Algebra.Graph.Labelled.AdjacencyMap.Internal as LAMI
-import Data.Map.Strict (Map, (!))
-import qualified Data.Map.Strict as Map
 import Data.List (minimumBy)
+import Data.Map.Strict (Map, (!), (\\))
+import qualified Data.Map.Strict as Map
 
 infinity :: Int
 infinity = maxBound
 
-dijkstraStep :: (Ord a) => (Map a Int, Map a Int) -> LAMI.AdjacencyMap Int a -> (Map a Int, Map a Int)
+dijkstra :: (Ord a) => a -> LAMI.AdjacencyMap Int a -> Maybe (Map a Int)
+dijkstra k m =
+  if Map.member k $ LAMI.adjacencyMap m
+    then Just $ recurseDijkstraStep (Map.singleton k 0, Map.empty)
+    else Nothing
+  where
+    recurseDijkstraStep (w, a)
+      | w == Map.empty = a
+      | otherwise = recurseDijkstraStep $ dijkstraStep (w, a) m
+
+dijkstraStep ::
+     (Ord a)
+  => (Map a Int, Map a Int)
+  -> LAMI.AdjacencyMap Int a
+  -> (Map a Int, Map a Int)
 dijkstraStep wA@(w, a) m =
   case minValuedElemKey w of
     Nothing -> (w, addUnreachableVertices a m)
     Just kV -> dijkstraStepNonEmpty wA m kV
 
-addUnreachableVertices :: (Ord a) => Map a Int -> LAMI.AdjacencyMap Int a -> Map a Int 
-addUnreachableVertices a m = unionMin a allVertices
+mapUnionMin :: (Ord a) => Map a Int -> Map a Int -> Map a Int
+mapUnionMin = Map.unionWith min'
+  where
+    min' a b
+      | a < b = a
+      | otherwise = b
+
+addUnreachableVertices ::
+     (Ord a) => Map a Int -> LAMI.AdjacencyMap Int a -> Map a Int
+addUnreachableVertices a m = mapUnionMin a allVertices
   where
     allVertices = Map.map (const infinity) $ LAMI.adjacencyMap m
-    unionMin = Map.unionWith min'
-    min' a b = if a < b then a else b
-    
-dijkstraStepNonEmpty :: (Ord a) => (Map a Int, Map a Int) -> LAMI.AdjacencyMap Int a -> (a, Int) -> (Map a Int, Map a Int)
+
+dijkstraStepNonEmpty ::
+     (Ord a)
+  => (Map a Int, Map a Int)
+  -> LAMI.AdjacencyMap Int a
+  -> (a, Int)
+  -> (Map a Int, Map a Int)
 dijkstraStepNonEmpty (w, a) m (k, v) = (nW, nA)
   where
-    nW = unionMin (Map.delete k w) (addWeight $ LAMI.adjacencyMap m ! k)
     nA = Map.insert k v a
-    unionMin = Map.unionWith min'
+    nW =
+      mapUnionMin (Map.delete k w) (addWeight $ LAMI.adjacencyMap m ! k) \\ nA
     addWeight = Map.map (+ v)
-    min' a b = if a < b then a else b
 
 minValuedElemKey :: (Ord v) => Map k v -> Maybe (k, v)
-minValuedElemKey m = 
+minValuedElemKey m =
   case Map.toList m of
     [] -> Nothing
     l -> Just $ minimumBy compare' l
   where
     compare' a b = compare (snd a) (snd b)
-    
