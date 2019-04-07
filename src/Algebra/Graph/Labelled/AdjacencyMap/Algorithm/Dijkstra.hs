00803d18a97686001362f5336dd943f88e549a54
adithyaov <111501017@smail.iitpkd.ac.in>
2019-04-08 03:50:25 +0530

    Changed the label type from Int to Distance from Algebra.Graph.Label

--- src/Algebra/Graph/Labelled/AdjacencyMap/Algorithm/Dijkstra.hs
+++ src/Algebra/Graph/Labelled/AdjacencyMap/Algorithm/Dijkstra.hs
@@ -4,14 +4,12 @@ import Algebra.Graph.Labelled.AdjacencyMap.Internal (AdjacencyMap(..))
 import Data.List (minimumBy)
 import Data.Map.Strict (Map, (!), (\\))
 import qualified Data.Map.Strict as Map
+import Algebra.Graph.Label (Semiring(..), Distance, infinite, distance)
 
-infinity :: Int
-infinity = maxBound
-
-dijkstra :: (Ord a) => a -> AdjacencyMap Int a -> Maybe (Map a Int)
+dijkstra :: (Ord a, Ord b, Num b) => a -> AdjacencyMap (Distance b) a -> Maybe (Map a (Distance b))
 dijkstra k m =
   if Map.member k $ adjacencyMap m
-    then Just $ recurseDijkstraStep (Map.singleton k 0, Map.empty)
+    then Just $ recurseDijkstraStep (Map.singleton k one, Map.empty)
     else Nothing
   where
     recurseDijkstraStep (w, a)
@@ -19,44 +17,44 @@ dijkstra k m =
       | otherwise = recurseDijkstraStep $ dijkstraStep (w, a) m
 
 dijkstraStep ::
-     (Ord a)
-  => (Map a Int, Map a Int)
-  -> AdjacencyMap Int a
-  -> (Map a Int, Map a Int)
+     (Ord a, Ord b, Num b)
+  => (Map a (Distance b), Map a (Distance b))
+  -> AdjacencyMap (Distance b) a
+  -> (Map a (Distance b), Map a (Distance b))
 dijkstraStep wA@(w, a) m =
   case minValuedElemKey w of
     Nothing -> (w, addUnreachableVertices a m)
     Just kV -> dijkstraStepNonEmpty wA m kV
 
-mapUnionMin :: (Ord a) => Map a Int -> Map a Int -> Map a Int
-mapUnionMin = Map.unionWith min'
+mapUnionMin :: (Ord a, Ord b) => Map a (Distance b) -> Map a (Distance b) -> Map a(Distance b)
+mapUnionMin = Map.unionWith min2
   where
-    min' a b
+    min2 a b
       | a < b = a
       | otherwise = b
 
 addUnreachableVertices ::
-     (Ord a) => Map a Int -> AdjacencyMap Int a -> Map a Int
+     (Ord a, Ord b) => Map a (Distance b) -> AdjacencyMap (Distance b) a -> Map a(Distance b)
 addUnreachableVertices a m = mapUnionMin a allVertices
   where
-    allVertices = Map.map (const infinity) $ adjacencyMap m
+    allVertices = Map.map (const $ distance infinite) $ adjacencyMap m
 
 dijkstraStepNonEmpty ::
-     (Ord a)
-  => (Map a Int, Map a Int)
-  -> AdjacencyMap Int a
-  -> (a, Int)
-  -> (Map a Int, Map a Int)
+     (Ord a, Num b, Ord b)
+  => (Map a (Distance b), Map a (Distance b))
+  -> AdjacencyMap (Distance b) a
+  -> (a, Distance b)
+  -> (Map a (Distance b), Map a (Distance b))
 dijkstraStepNonEmpty (w, a) m (k, v) = (nW, nA)
   where
     nA = Map.insert k v a
     nW = mapUnionMin (Map.delete k w) (addWeight $ adjacencyMap m ! k) \\ nA
-    addWeight = Map.map (+ v)
+    addWeight = Map.map (<.> v)
 
 minValuedElemKey :: (Ord v) => Map k v -> Maybe (k, v)
 minValuedElemKey m =
   case Map.toList m of
     [] -> Nothing
-    l -> Just $ minimumBy compare' l
+    l -> Just $ minimumBy compareSnd l
   where
-    compare' a b = compare (snd a) (snd b)
+    compareSnd a b = compare (snd a) (snd b)
