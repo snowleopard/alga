b7e8b1a0b53b0f0ee7c6bab1b6045167c5703b7e
adithyaov <111501017@smail.iitpkd.ac.in>
2019-05-01 20:02:32 +0530

    Added explanations for the functions. Need to find an effictive heap to work with.

--- src/Algebra/Graph/Labelled/AdjacencyMap/Algorithm/Dijkstra.hs
+++ src/Algebra/Graph/Labelled/AdjacencyMap/Algorithm/Dijkstra.hs
@@ -13,6 +13,8 @@ type DijkstraForDistance a b = DijkstraState FstMinPolicy a (Distance b)
 
 type DijkstraForCapacity a b = DijkstraState FstMaxPolicy a (Capacity b)
 
+-- | `dijkstra` is the main function using bothe `initialize` and `exploreVertices`.
+-- The time complexity of `dijkstra` is O(|V| + |V| * |E|) = O(|V| * |E|)
 dijkstra ::
      (Eq e, Ord a, HeapItem p (e, a), Semiring e)
   => a
@@ -33,11 +35,14 @@ instance (Show e, Show a) => Show (DijkstraState p a e) where
   show (DijkstraState heap' distance' path') =
     "distance: " ++ show distance' ++ "\n" ++ "path: " ++ show path'
 
+-- | This is a helper function to work with the heap in DijkstraState
 putHeap :: Heap p (e, a) -> State (DijkstraState p a e) ()
 putHeap h = do
   (DijkstraState _ distance' path') <- get
   put $ DijkstraState h distance' path'
 
+-- | `initializes` gives the initial state for the State computation.
+-- The time complexity is O(|V|)
 initialize ::
      (Ord a, HeapItem p (e, a), Semiring e)
   => a
@@ -50,6 +55,9 @@ initialize a am =
     (Map.insert a one . Map.map (const zero) $ adjacencyMap am)
     (Map.map (const Nothing) $ adjacencyMap am)
 
+-- | `exploreVertices` is an iterative function which explores all the vertices.
+-- The time complexity is O(|V| * |E|).
+-- O(|E|) is due to the usage of `exploreVertex`.
 exploreVertices ::
      (Ord a, Eq e, HeapItem p (e, a), Semiring e)
   => AdjacencyMap e a
@@ -63,6 +71,8 @@ exploreVertices am = do
       exploreVertex v am
       exploreVertices am
 
+-- | `exploreVertex` explores the edges of a single vertex.
+-- The time complexity is hence O(|E|).
 exploreVertex ::
      (Ord a, Semiring e, Eq e, HeapItem p (e, a))
   => a
@@ -72,6 +82,12 @@ exploreVertex v1 am =
   sequence_ $
   Map.mapWithKey (\v2 e -> exploreEdge e v1 v2) (adjacencyMap am ! v1)
 
+-- | This is a helper function for modifying the weight of an element in the heap.
+-- The current heap library does not provide a way to modify or delete any value.
+-- Hence we first delete the value using the filter function and then we insert the value.
+-- The time complexity of `Heap.filter` if O(|V|).
+-- The time complexity of `Heap.insert` is O(log|V|).
+-- The time complexity of `modifyHeapItemPrio` is O(|V|).
 modifyHeapItemPrio ::
      (HeapItem p (e, a), Eq a) => a -> e -> Heap p (e, a) -> Heap p (e, a)
 modifyHeapItemPrio vertex prio curHeap =
@@ -79,6 +95,9 @@ modifyHeapItemPrio vertex prio curHeap =
   where
     isNot v (_, i) = v /= i
 
+-- | `exploreEdge` takes an edge (v1 -> v2) with weight e.
+-- It computes a new distance value for v2 and changes it in the heap.
+-- The time complexity for this function is O(|V|), this is directly dependent on `modifyHeapItemPrio`.
 exploreEdge ::
      (HeapItem p (e, a), Ord a, Semiring e, Eq e)
   => e
