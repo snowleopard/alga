e6ae07b5364e04f4b63c5403025ecc649582572e
adithyaov <111501017@smail.iitpkd.ac.in>
2019-05-03 01:37:22 +0530

    Made the simple version of Dijkstra
    As suggested in the PR#194, I've made a simple version
    of Dijkstra using the Data.Set for the heap. This version
    only works for the Distance semigroup.

--- src/Algebra/Graph/Labelled/AdjacencyMap/Algorithm/Dijkstra.hs
+++ src/Algebra/Graph/Labelled/AdjacencyMap/Algorithm/Dijkstra.hs
@@ -1,63 +1,55 @@
 module Algebra.Graph.Labelled.AdjacencyMap.Algorithm.Dijkstra where
 
-import Algebra.Graph.Label (Capacity, Distance, Semiring(..), (<+>), zero)
+import Algebra.Graph.Label (Distance, Semiring(..), (<+>), zero)
 import Algebra.Graph.Labelled.AdjacencyMap.Internal (AdjacencyMap(..))
 import Control.Monad (sequence_, when)
 import Control.Monad.Trans.State.Strict (State, execState, get, gets, put)
-import Data.Heap (FstMaxPolicy, FstMinPolicy, Heap, HeapItem(..))
-import qualified Data.Heap as Heap
 import Data.Map (Map, (!))
 import qualified Data.Map.Strict as Map
+import Data.Set (Set)
+import qualified Data.Set as Set
 
-type DijkstraForDistance a b = DijkstraState FstMinPolicy a (Distance b)
-
-type DijkstraForCapacity a b = DijkstraState FstMaxPolicy a (Capacity b)
-
--- | /O(|V| + |E| * log|V|) `dijkstra` is the main function using
+-- | /O(|V| + |E| * log|V|)/ `dijkstra` is the main function using
 -- both `initialize` and `exploreVertices`.
 dijkstra ::
-     (Eq e, Ord a, HeapItem p (e, a), Semiring e)
+     (Ord a, Num e, Ord e)
   => a
-  -> AdjacencyMap e a
-  -> DijkstraState p a e
+  -> AdjacencyMap (Distance e) a
+  -> DijkstraState a (Distance e)
 dijkstra a am =
-  flip execState (DijkstraState Heap.empty Map.empty Map.empty) $ do
+  flip execState (DijkstraState Set.empty Map.empty Map.empty) $ do
     initialize a am
     exploreVertices am
 
-data DijkstraState p a e = DijkstraState
-  { heap :: Heap p (e, a)
+data DijkstraState a e = DijkstraState
+  { heap :: Set (e, a)
   , distance :: Map a e
   , path :: Map a (Maybe a)
-  }
-
-instance (Show e, Show a) => Show (DijkstraState p a e) where
-  show (DijkstraState _ distance' path') =
-    "distance: " ++ show distance' ++ "\n" ++ "path: " ++ show path'
+  } deriving (Show)
 
 -- | /O(|V|)/ `initialize` gives the initial state for the
 -- State computation.
 initialize ::
-     (Ord a, HeapItem p (e, a), Semiring e)
+     (Ord a, Num e, Ord e)
   => a
-  -> AdjacencyMap e a
-  -> State (DijkstraState p a e) ()
+  -> AdjacencyMap (Distance e) a
+  -> State (DijkstraState a (Distance e)) ()
 initialize a am =
   put $
   DijkstraState
-    (Heap.singleton (one, a))
+    (Set.singleton (one, a))
     (Map.insert a one . Map.map (const zero) $ adjacencyMap am)
     (Map.map (const Nothing) $ adjacencyMap am)
 
 -- | /O(|E| * log|V|)/ `exploreVertices` is an iterative function which explores all the vertices.
 exploreVertices ::
-     (Ord a, Eq e, HeapItem p (e, a), Semiring e)
-  => AdjacencyMap e a
-  -> State (DijkstraState p a e) ()
+     (Ord a, Ord e, Num e)
+  => AdjacencyMap (Distance e) a
+  -> State (DijkstraState a (Distance e)) ()
 exploreVertices am = do
   curHeap <- gets heap
   curS <- get
-  case Heap.view curHeap of
+  case Set.minView curHeap of
     Nothing -> return ()
     (Just ((_, v), t)) -> do
       put $ curS {heap = t}
@@ -66,10 +58,10 @@ exploreVertices am = do
 
 -- | /O(log|V|)/ `exploreVertex` explores the edges of a single vertex.
 exploreVertex ::
-     (Ord a, Semiring e, Eq e, HeapItem p (e, a))
+     (Ord a, Ord e, Num e)
   => a
-  -> AdjacencyMap e a
-  -> State (DijkstraState p a e) ()
+  -> AdjacencyMap (Distance e) a
+  -> State (DijkstraState a (Distance e)) ()
 exploreVertex v1 am =
   sequence_ $
   Map.mapWithKey (\v2 e -> exploreEdge e v1 v2) (adjacencyMap am ! v1)
@@ -78,11 +70,11 @@ exploreVertex v1 am =
 -- It computes a new distance value for v2 and inserts it in the heap
 -- if not explored.
 exploreEdge ::
-     (HeapItem p (e, a), Ord a, Semiring e, Eq e)
-  => e
+     (Ord a, Ord e, Num e)
+  => Distance e
   -> a
   -> a
-  -> State (DijkstraState p a e) ()
+  -> State (DijkstraState a (Distance e)) ()
 exploreEdge e v1 v2 = do
   curHeap <- gets heap
   curDistance <- gets distance
@@ -93,8 +85,6 @@ exploreEdge e v1 v2 = do
   when (newDV2 /= curDV2) $
     put $
     DijkstraState
-      (Heap.insert (newDV2, v2) curHeap)
+      (Set.insert (newDV2, v2) curHeap)
       (Map.insert v2 newDV2 curDistance)
       (Map.insert v2 (Just v1) curPath)
-
-
