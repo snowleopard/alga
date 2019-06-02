6fe1f25983e8f5a2b61f3e0638688209134663ff
Vasily Alferov <ya-ikmik2012@yandex.ru>
2019-06-02 14:39:54 +0300

    Small fix for small patch
    1. Simplify consistency check for bipartite adjacency maps.
    2. Change Bipartite.AdjacencyMap.hasEdge signature to match other Alga
    modules.

--- src/Algebra/Graph/Bipartite/AdjacencyMap.hs
+++ src/Algebra/Graph/Bipartite/AdjacencyMap.hs
@@ -1,7 +1,10 @@
 module Algebra.Graph.Bipartite.AdjacencyMap (
     AdjacencyMap, leftToRight, rightToLeft,
     consistent,
-    ) where
+    hasEdge,
+) where
+
+import Data.Tuple
 
 import qualified Data.Map.Strict as Map
 import qualified Data.Set        as Set
@@ -12,13 +15,10 @@ data AdjacencyMap a b = BAM {
 } deriving Eq
 
 consistent :: (Ord a, Ord b) => AdjacencyMap a b -> Bool
-consistent (BAM lr rl) = oneWayConstistent lr rl && oneWayConstistent rl lr
+consistent (BAM lr rl) = internalEdgeList lr == (map swap $ internalEdgeList rl)
 
 internalEdgeList :: Map.Map a (Set.Set b) -> [(a, b)]
 internalEdgeList lr = [ (u, v) | (u, vs) <- Map.toAscList lr, v <- Set.toAscList vs ]
 
-hasEdge :: (Ord a, Ord b) => Map.Map a (Set.Set b) -> a -> b -> Bool
-hasEdge m u v = ((Set.member v) <$> (m Map.!? u)) == Just True
-
-oneWayConstistent :: (Ord a, Ord b) => Map.Map a (Set.Set b) -> Map.Map b (Set.Set a) -> Bool
-oneWayConstistent straight inverse = foldr (&&) True $ (uncurry . flip) (hasEdge inverse) <$> internalEdgeList straight
+hasEdge :: (Ord a, Ord b) => a -> b -> AdjacencyMap a b -> Bool
+hasEdge u v (BAM m _) = ((Set.member v) <$> (u `Map.lookup` m)) == Just True
