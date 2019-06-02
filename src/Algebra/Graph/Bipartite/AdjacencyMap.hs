d00b8c24c2b570ab1c312a022a22b0d11ad76bb8
Vasily Alferov <ya-ikmik2012@yandex.ru>
2019-06-02 19:15:58 +0300

    Add Bipartite.AdjacencyMap construction functions

--- src/Algebra/Graph/Bipartite/AdjacencyMap.hs
+++ src/Algebra/Graph/Bipartite/AdjacencyMap.hs
@@ -1,10 +1,14 @@
 module Algebra.Graph.Bipartite.AdjacencyMap (
     AdjacencyMap, leftToRight, rightToLeft,
     consistent,
+    fromAdjacencyMap, fromGraph,
     hasEdge,
 ) where
 
 import Data.Tuple
+
+import qualified Algebra.Graph              as G
+import qualified Algebra.Graph.AdjacencyMap as AM
 
 import qualified Data.Map.Strict as Map
 import qualified Data.Set        as Set
@@ -22,3 +26,13 @@ internalEdgeList lr = [ (u, v) | (u, vs) <- Map.toAscList lr, v <- Set.toAscList
 
 hasEdge :: (Ord a, Ord b) => a -> b -> AdjacencyMap a b -> Bool
 hasEdge u v (BAM m _) = ((Set.member v) <$> (u `Map.lookup` m)) == Just True
+
+fromAdjacencyMap :: (Ord a, Ord b) => AM.AdjacencyMap (Either a b) -> AdjacencyMap a b
+fromAdjacencyMap m = BAM (Map.fromAscList [ (u, filterRight vs) | (Left  u, vs) <- Map.toAscList (AM.adjacencyMap m)])
+                         (Map.fromAscList [ (u, filterLeft  vs) | (Right u, vs) <- Map.toAscList (AM.adjacencyMap m)])
+    where
+        filterRight s = Set.fromAscList [ v | Right v <- Set.toAscList s ]
+        filterLeft  s = Set.fromAscList [ v | Left  v <- Set.toAscList s ]
+
+fromGraph :: (Ord a, Ord b) => G.Graph (Either a b) -> AdjacencyMap a b
+fromGraph = fromAdjacencyMap . (G.foldg AM.empty AM.vertex AM.overlay AM.connect)
