2f78973ff06356735cbfdaabe67e24f88c03bbe3
jitwit <51518420+jitwit@users.noreply.github.com>
2019-08-03 19:56:41 -0400

    Fix BFS complexity bounds (#227)

--- src/Algebra/Graph/AdjacencyIntMap/Algorithm.hs
+++ src/Algebra/Graph/AdjacencyIntMap/Algorithm.hs
@@ -14,6 +14,10 @@
 --
 -- This module provides basic graph algorithms, such as /depth-first search/,
 -- implemented for the "Algebra.Graph.AdjacencyIntMap" data type.
+--
+-- Some of the worst-case complexities include the term /min(n,W)/.
+-- Following 'IntSet.IntSet' and 'IntMap.IntMap', the /W/ stands for
+-- word size (usually 32 or 64 bits).
 -----------------------------------------------------------------------------
 module Algebra.Graph.AdjacencyIntMap.Algorithm (
     -- * Algorithms
@@ -35,8 +39,11 @@ import qualified Data.Graph.Typed   as Typed
 import qualified Data.IntMap.Strict as IntMap
 import qualified Data.IntSet        as IntSet
 
--- | Compute the forest of a graph's vertices in breadth first order. Complexity:
--- /O(n+m*log n)/ time and /O(n+m)/ space.
+-- | Compute the /breadth-first search/ forest of a graph, such that
+--   adjacent vertices are explored in increasing order with respect
+--   to their 'Ord' instance.
+-- 
+--   Complexity: /O((n+m)*min(n,W))/ time and /O(n)/ space.
 --
 -- @
 -- bfsForest 'empty'                         == []
@@ -57,10 +64,10 @@ bfsForest :: AdjacencyIntMap -> Forest Int
 bfsForest g = bfsForestFrom' (vertexList g) g
 
 -- | Like 'bfsForest', but the traversal is seeded by a list of
--- vertices. Vertices not in the graph are ignored.
+--   vertices. Vertices not in the graph are ignored.
 --
--- Let /L/ be the number of seed vertices. Complexity: /O(n+(L+m)*logn)/
--- time and /O(n+m)/ space.
+--   Let /L/ be the number of seed vertices. Complexity:
+--   /O((L+m)*min(n,W))/ time and /O(n)/ space.
 --
 -- @
 -- 'forest' (bfsForestFrom [1,2] $ 'edge' 1 2)      == 'vertices' [1,2]
@@ -80,7 +87,7 @@ bfsForest g = bfsForestFrom' (vertexList g) g
 -- 
 -- @
 bfsForestFrom :: [Int] -> AdjacencyIntMap -> Forest Int
-bfsForestFrom vs g = bfsForestFrom' [ v | v <- vs, hasVertex v g] g
+bfsForestFrom vs g = bfsForestFrom' [ v | v <- vs, hasVertex v g ] g
 
 bfsForestFrom' :: [Int] -> AdjacencyIntMap -> Forest Int
 bfsForestFrom' vs g = evalState (explore vs) IntSet.empty where
@@ -97,11 +104,11 @@ bfsForestFrom' vs g = evalState (explore vs) IntSet.empty where
 -- | Like 'bfsForestFrom' with the resulting forest converted to a
 --   level structure.  Flattening the result via @'concat' . 'bfs' vs@
 --   gives an enumeration of vertices reachable from @vs@ in breadth
---   first order. Adjacent nodes are expanded smallest to biggest
---   according to the 'Ord' instance for 'Int'.
+--   first order. Adjacent vertices are explored in increasing order
+--   with respect to their 'Ord' instance.
 --
 --   Let /L/ be the number of seed vertices. Complexity:
---   /O(n+(L+m)*log n)/ time and /O(n+m)/ space.
+--   /O((L+m)*min(n,W))/ time and /O(n)/ space.
 -- 
 -- @
 -- bfs vs 'empty'                                         == []
