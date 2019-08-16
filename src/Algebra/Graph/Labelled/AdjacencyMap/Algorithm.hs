801abddfbe9b98065b4b34847fba86d5ab52ed40
adithyaov <adi.obilisetty@gmail.com>
2019-08-16 14:09:19 +0530

    Made prim's algorithm

--- src/Algebra/Graph/Labelled/AdjacencyMap/Algorithm.hs
+++ src/Algebra/Graph/Labelled/AdjacencyMap/Algorithm.hs
@@ -1,9 +1,26 @@
 module Algebra.Graph.Labelled.AdjacencyMap.Algorithm where
 
-import Algebra.Graph.Label
-import Data.Map.Strict (Map, (!))
+import Data.Map.Strict ((!))
 import Algebra.Graph.Labelled.AdjacencyMap
+import Data.List (foldl', sort)
 
-import qualified Data.Set as Set
 import qualified Data.Map.Strict as Map
+
+-- TODO: Make Prim's more efficient
+-- TODO: Improve documentation for prim
+-- TODO: Write examples and tests for prim
+-- TODO: Remove Monoid instance on edges in Prim's
+prim :: (Ord a, Ord e, Monoid e) => AdjacencyMap e a -> AdjacencyMap e a
+prim wam = edges . snd . foldl' addEdge (im, []) $ eL
+  where
+    am = adjacencyMap wam
+    im = Map.mapWithKey const am
+    eL = sort . edgeList $ wam
+    makeSet x y = Map.adjust (min x) y . Map.adjust (min y) x
+    findSet m x = if m ! x == x
+                     then x
+                     else findSet m $ m ! x
+    addEdge (m, g) e@(_, x, y) = if findSet m x == findSet m y
+                                    then (m, g)
+                                    else (makeSet x y m, e:g)
 
