c7edeb649191cbb130c4a860c9536f018e4ec557
adithyaov <adi.obilisetty@gmail.com>
2019-08-16 16:18:58 +0530

    Minor changes

--- src/Algebra/Graph/Labelled/AdjacencyMap/Algorithm.hs
+++ src/Algebra/Graph/Labelled/AdjacencyMap/Algorithm.hs
@@ -10,13 +10,17 @@ import qualified Data.Map.Strict as Map
 -- TODO: Improve documentation for prim
 -- TODO: Write examples and tests for prim
 -- TODO: Remove Monoid instance on edges in Prim's
+-- TODO: Handel disconnected components properly
+-- | Prim's algorithm to find the minimum spanning tree.
+-- Given a graph 'g', 'prim' returns a new graph 'g\'' which is
+-- the MST (minimum spanning tree) of graph 'g'.
 prim :: (Ord a, Ord e, Monoid e) => AdjacencyMap e a -> AdjacencyMap e a
 prim wam = edges . snd . foldl' addEdge (im, []) $ eL
   where
     am = adjacencyMap wam
     im = Map.mapWithKey const am
     eL = sort . edgeList $ wam
-    makeSet x y = Map.adjust (min x) y . Map.adjust (min y) x
+    makeSet x = Map.adjust (const x)
     findSet m x = if m ! x == x
                      then x
                      else findSet m $ m ! x
