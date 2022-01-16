af85c31e54496292f0c285db96187e977e7b7fcd
chenzizhao <zoe.chenzizhao@gmail.com>
2022-01-16 14:36:09 -0500

    Remove trailing space and some new lines

--- test/Algebra/Graph/Test/Example/Todo.hs
+++ test/Algebra/Graph/Test/Example/Todo.hs
@@ -11,27 +11,26 @@ import Algebra.Graph.Example.Todo
 testTodo :: IO ()
 testTodo = do
     putStrLn "\n============ Example.Todo (Holiday) ============"
-    
     test "A todo list is semantically Maybe [a]" $
         todo ("presents" :: Todo String) == Just ["presents"]
-    
+
     test "The overlay operator (+) adds non-dependent items to the todo list" $
         todo ("coat" + "presents" :: Todo String) == Just ["coat", "presents"]
-    
+
     test "The connect operator (*) adds dependency between items" $
-        let 
+        let
             shopping :: Todo String = "presents" + "coat" + "scarf"
             holiday :: Todo String = shopping * "pack" * "travel"
         in todo (holiday + "scarf" * "coat")
             == Just ["presents","scarf","coat", "pack","travel"]
 
     test "Contradictory constraints make the todo list impossible to schedule" $
-        let 
+        let
             shopping :: Todo String = "presents" + "coat" + "scarf"
             holiday :: Todo String = shopping * "pack" * "travel"
         in todo (holiday + "travel" * "presents") == Nothing
-    
-    test "Introduce item priority to schedule the todo list" $ 
+
+    test "Introduce item priority to schedule the todo list" $
         let
             shopping :: Todo String
             shopping = "presents" + "coat" + low "phone wife" * "scarf"
@@ -50,13 +49,12 @@ testTodo = do
             == Just ["presents","phone wife","scarf","coat","pack","travel"]
 
     putStrLn "\n============ Example.Todo (Commandline) ============"
-
     test "The pull connect operator maintains command line semantics" $
         let
             cmdl :: Todo String
             cmdl = "gcc" * ("-c" ~*~ "src.c" + "-o" ~*~ "src.o")
         in todo cmdl == Just ["gcc","-c","src.c","-o","src.o"]
-    
+
     test "Swapping flags are allowed by the commutative overlay opeartor" $
         let
             cmdl1 :: Todo String
@@ -64,12 +62,12 @@ testTodo = do
             cmdl2 :: Todo String
             cmdl2 = "gcc" * ("-o" ~*~ "src.o" + "-c" ~*~ "src.c")
         in cmdl1 == cmdl2
-    
+
     test "The usual connect operator breaks semantics" $
         let
             cmdl :: Todo String
             cmdl = "gcc" * ("-c" * "src.c" + "-o" * "src.o")
-        in 
+        in
             todo cmdl == Just ["gcc","-c","-o","src.c","src.o"]
 
     test "Transform command lines by adding optimisation flag" $
@@ -79,5 +77,5 @@ testTodo = do
             optimise :: Int -> Todo String -> Todo String
             optimise level = (* flag)
                 where flag = vertex $ "-O" ++ show level
-        in todo (optimise 2 cmdl) == 
+        in todo (optimise 2 cmdl) ==
             Just ["gcc","-c","src.c","-o","src.o","-O2"]
