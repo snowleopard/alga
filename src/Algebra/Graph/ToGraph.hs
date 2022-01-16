399b5dc538a2496b67f9322dc7247e06a4ec326b
Andrey Mokhov <andrey.mokhov@gmail.com>
2022-01-16 18:17:17 +0000

    Minor updates (#287)
    * Update copyright year
    
    * Update ci.yml
    
    * Update badge link

--- src/Algebra/Graph/ToGraph.hs
+++ src/Algebra/Graph/ToGraph.hs
@@ -2,7 +2,7 @@
 -----------------------------------------------------------------------------
 -- |
 -- Module     : Algebra.Graph.ToGraph
--- Copyright  : (c) Andrey Mokhov 2016-2021
+-- Copyright  : (c) Andrey Mokhov 2016-2022
 -- License    : MIT (see the file LICENSE)
 -- Maintainer : andrey.mokhov@gmail.com
 -- Stability  : experimental
--- test/Algebra/Graph/Test/AdjacencyIntMap.hs
+++ test/Algebra/Graph/Test/AdjacencyIntMap.hs
@@ -1,7 +1,7 @@
 -----------------------------------------------------------------------------
 -- |
 -- Module     : Algebra.Graph.Test.AdjacencyIntMap
--- Copyright  : (c) Andrey Mokhov 2016-2021
+-- Copyright  : (c) Andrey Mokhov 2016-2022
 -- License    : MIT (see the file LICENSE)
 -- Maintainer : andrey.mokhov@gmail.com
 -- Stability  : experimental
