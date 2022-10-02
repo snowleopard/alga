fcf051181d0646b66168140d81a031b75ffaa163
Andrey Mokhov <andrey.mokhov@gmail.com>
2022-10-02 11:25:44 +0100

    Add example todo lists, tweak formatting

--- src/Algebra/Graph/Example/Todo.hs
+++ src/Algebra/Graph/Example/Todo.hs
@@ -1,5 +1,13 @@
 {-# LANGUAGE OverloadedStrings #-}
-module Algebra.Graph.Example.Todo (Todo, todo, low, high, (~*~), (>*<), priority) where
+module Algebra.Graph.Example.Todo (
+    -- * Creating and manipulating to-do lists
+    Todo, todo, low, high, (~*~), (>*<), priority,
+
+    -- * Examples
+    shopping, holiday
+    ) where
+
+-- Based on https://blogs.ncl.ac.uk/andreymokhov/graphs-in-disguise/
 
 import Data.Map (Map)
 import Data.String
@@ -13,10 +21,10 @@ import qualified Data.Map as Map
 data Todo a = T (Map a Int) (AdjacencyMap a) deriving Show
 
 instance Ord a => Eq (Todo a) where
-  x == y = todo x == todo y
+    x == y = todo x == todo y
 
 instance (IsString a, Ord a) => IsString (Todo a) where
-  fromString = C.vertex . fromString
+    fromString = C.vertex . fromString
 
 -- Lower the priority of items in a given todo list
 low :: Todo a -> Todo a
@@ -39,8 +47,7 @@ x ~*~ y = low x `C.connect` high y
 x >*< y = high x `C.connect` low y
 
 todo :: forall a. Ord a => Todo a -> Maybe [a]
-todo (T p g) =
-  case AM.topSort $ gmap prioritise g of
+todo (T p g) = case AM.topSort $ gmap prioritise g of
     Left _ -> Nothing
     Right xs -> Just $ map snd xs
   where
@@ -48,16 +55,26 @@ todo (T p g) =
     prioritise x = (negate $ Map.findWithDefault 0 x p, x)
 
 instance (IsString a, Ord a) => Num (Todo a) where
-  fromInteger i = fromString $ show (fromInteger i :: Integer)
-  (+)           = C.overlay
-  (*)           = C.connect
-  signum        = const C.empty
-  abs           = id
-  negate        = id
+    fromInteger i = fromString $ show (fromInteger i :: Integer)
+    (+)           = C.overlay
+    (*)           = C.connect
+    signum        = const C.empty
+    abs           = id
+    negate        = id
 
 instance Ord a => Graph (Todo a) where
-  type Vertex (Todo a) = a
-  empty    = T Map.empty AM.empty
-  vertex x = T (Map.singleton x 0) (C.vertex x)
-  overlay (T p1 g1) (T p2 g2) = T (Map.unionWith (+) p1 p2) (C.overlay g1 g2)
-  connect (T p1 g1) (T p2 g2) = T (Map.unionWith (+) p1 p2) (C.connect g1 g2)
+    type Vertex (Todo a) = a
+    empty    = T Map.empty AM.empty
+    vertex x = T (Map.singleton x 0) (C.vertex x)
+    overlay (T p1 g1) (T p2 g2) = T (Map.unionWith (+) p1 p2) (C.overlay g1 g2)
+    connect (T p1 g1) (T p2 g2) = T (Map.unionWith (+) p1 p2) (C.connect g1 g2)
+
+-- λ> todo shopping
+-- Just ["coat","presents","phone wife","scarf"]
+shopping :: Todo String
+shopping = "presents" + "coat" + "phone wife" ~*~ "scarf"
+
+-- λ> todo holiday
+-- Just ["coat","presents","phone wife","scarf","pack","travel"]
+holiday :: Todo String
+holiday = shopping * "pack" * "travel"
