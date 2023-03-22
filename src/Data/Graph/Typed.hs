3829d47dac7d721cc23f7ab16ece3eeddd797cfc
Andrey Mokhov <andrey.mokhov@gmail.com>
2023-03-22 14:25:34 +0700

    Bump year

--- src/Data/Graph/Typed.hs
+++ src/Data/Graph/Typed.hs
@@ -1,7 +1,7 @@
 -----------------------------------------------------------------------------
 -- |
 -- Module     : Data.Graph.Typed
--- Copyright  : (c) Anton Lorenzen, Andrey Mokhov 2016-2022
+-- Copyright  : (c) Anton Lorenzen, Andrey Mokhov 2016-2023
 -- License    : MIT (see the file LICENSE)
 -- Maintainer : anfelor@posteo.de, andrey.mokhov@gmail.com
 -- Stability  : unstable
