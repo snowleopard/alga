b2d0e2b345d43f6776cd462e238e15af98f8ce6d
Andrey Mokhov <andrey.mokhov@gmail.com>
2025-07-13 17:57:41 +0800

    Fix Linux CI (#320)

--- src/Algebra/Graph/Labelled/AdjacencyMap.hs
+++ src/Algebra/Graph/Labelled/AdjacencyMap.hs
@@ -1,7 +1,7 @@
 -----------------------------------------------------------------------------
 -- |
 -- Module     : Algebra.Graph.Labelled.AdjacencyMap
--- Copyright  : (c) Andrey Mokhov 2016-2024
+-- Copyright  : (c) Andrey Mokhov 2016-2025
 -- License    : MIT (see the file LICENSE)
 -- Maintainer : andrey.mokhov@gmail.com
 -- Stability  : experimental
